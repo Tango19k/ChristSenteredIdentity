0a193264f17b57dab3781325c312d00e594002cb
Tango19k <sespinal95@hotmail.com>
2025-02-27 19:20:14 -0500

    Prepare for Heroku deployment

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, world!"
+    return "Christ SENTered Identity"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    
+    app.run(debug=True)
